d90b48d62e8324d2713949d5d45e1b53aba991a6
swatikar95 <swati.cse.ruet@gmail.com>
2024-03-11 10:55:02 -0400

    combined twitter and linkedin

--- ice_breaker_link_twit.py
+++ ice_breaker_link_twit.py
@@ -15,29 +15,28 @@ if __name__ == "__main__":
     load_dotenv()
     print("hello")
     os.environ.get("OPENAI_API_KEY")
+  
+
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linked_data = scrape_linked_profile(linked_profile_url=linkedin_profile_url)
 
     twitter_user_name = twitter_lookup_agent(name=name)
     tweets = scrape_user_tweets(username=twitter_user_name,num_tweets=2)
 
     summary_template = """
-    given the information {information} about a person I want you to create:
+    given the linkedin information {linkedin_information} and twitter information {twitter_information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about the
     3. A topic that may interest them
     4. 2 creative Ice breakers to open a conversation with them
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"],template=summary_template)
+    summary_prompt_template = PromptTemplate(input_variables=["linkedin_information","twitter_information"],template=summary_template)
     llm = ChatOpenAI(temperature=0,model_name="gpt-3.5-turbo") 
 
     chain = LLMChain(llm=llm,prompt=summary_prompt_template)
     
     
-    # tweet_data = scrape_user_tweets(
-    #     twitter_profile_url = tweets
-    # )
-    # print(chain.run(information=linked_data))
-
-    print(chain.run(information=tweets))
+    print(chain.run(linked_information=linked_data,twitter_information=tweets))
 
 
